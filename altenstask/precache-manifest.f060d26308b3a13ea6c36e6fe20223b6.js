b1c577ed4e205737dc42a3f5f287134eccd823a2
mregydev <nodeisworth@gmail.com>
2019-10-31 16:34:57 +0300

    added altens task

---
+++ altenstask/precache-manifest.f060d26308b3a13ea6c36e6fe20223b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4fa48e39c81e6cc80caa9df753a0a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52b46a018bae0463d8fc",
+    "url": "/static/css/main.f1ed1468.chunk.css"
+  },
+  {
+    "revision": "694ba549d98a365a421f",
+    "url": "/static/js/2.b8ab5b15.chunk.js"
+  },
+  {
+    "revision": "52b46a018bae0463d8fc",
+    "url": "/static/js/main.5ba7e4b4.chunk.js"
+  },
+  {
+    "revision": "c8c9bf962cc02faf3cca",
+    "url": "/static/js/runtime-main.0ef20e4f.js"
+  }
+]);
