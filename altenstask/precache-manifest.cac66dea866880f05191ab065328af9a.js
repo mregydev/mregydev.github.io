21a5160e122321fe59503e3da84d527654cdbb41
mregydev <nodeisworth@gmail.com>
2019-11-04 23:13:24 +0300

    updated altens task

---
+++ altenstask/precache-manifest.cac66dea866880f05191ab065328af9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60fe4ae13dedc334ac53a21d293f076f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "314a3d1f5c15b2ef18fc",
+    "url": "./static/css/main.c0ea73d5.chunk.css"
+  },
+  {
+    "revision": "c181c742cb28fc717de8",
+    "url": "./static/js/2.7abaecab.chunk.js"
+  },
+  {
+    "revision": "314a3d1f5c15b2ef18fc",
+    "url": "./static/js/main.51609422.chunk.js"
+  },
+  {
+    "revision": "d20c7b8258f3b0ecbe15",
+    "url": "./static/js/runtime-main.7f9b8e34.js"
+  }
+]);
--- altenstask/service-worker.js
+++ altenstask/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f060d26308b3a13ea6c36e6fe20223b6.js"
+  "./precache-manifest.cac66dea866880f05191ab065328af9a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
