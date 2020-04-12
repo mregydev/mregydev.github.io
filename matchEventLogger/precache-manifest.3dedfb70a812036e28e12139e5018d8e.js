3ecbae96b3aaac2701b3768c9c4d7690163b0613
unknown <mregydev@gmail.com>
2020-04-12 15:08:42 +0300

    added match event logger

---
+++ matchEventLogger/precache-manifest.3dedfb70a812036e28e12139e5018d8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6dbbb0431a109f5b1e73f79a8d40a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5615538ead64ca7e215",
+    "url": "/static/css/main.4db1fbc4.chunk.css"
+  },
+  {
+    "revision": "46c98385d06dcd9c5c56",
+    "url": "/static/js/2.c2c959a8.chunk.js"
+  },
+  {
+    "revision": "35a08d2e06945b779fadacb826071be3",
+    "url": "/static/js/2.c2c959a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5615538ead64ca7e215",
+    "url": "/static/js/main.0f0e3095.chunk.js"
+  },
+  {
+    "revision": "6c177b0fd9b2ffc1b041",
+    "url": "/static/js/runtime-main.3a5d97f8.js"
+  },
+  {
+    "revision": "5ea912385da5a0ad2f3f107ba9036571",
+    "url": "/static/media/back.5ea91238.jpg"
+  }
+]);
