310d00534ed8f95fa1ac2bdf8da8df3f502b72fd
unknown <mregydev@gmail.com>
2020-05-07 00:53:38 +0300

    added buseet dashboard

---
+++ buseet/precache-manifest.c694ce8f7598bd11d648461525299a4d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041ced46e5df3bf337cce856cd88a5e0",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "86c2b5535eda7f018ff1",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "cc7766eaf161435e7e2b",
+    "url": "/material-dashboard-react/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "86c2b5535eda7f018ff1",
+    "url": "/material-dashboard-react/static/js/2.a9ab1b12.chunk.js"
+  },
+  {
+    "revision": "cc7766eaf161435e7e2b",
+    "url": "/material-dashboard-react/static/js/main.6dded19a.chunk.js"
+  },
+  {
+    "revision": "6a0cfeb8a7bb3dca7439",
+    "url": "/material-dashboard-react/static/js/runtime~main.21de15b8.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
