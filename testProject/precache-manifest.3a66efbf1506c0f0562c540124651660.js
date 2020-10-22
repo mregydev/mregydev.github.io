a0ba673a75fcb06779c3439b2ceffdce10c2905f
mregydev <mregydev@gmail.com>
2020-10-22 17:52:49 +0200

    created test project
    created test project

---
+++ testProject/precache-manifest.3a66efbf1506c0f0562c540124651660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c827e124cbc3baee6d967199c01da94",
+    "url": "/testProject/index.html"
+  },
+  {
+    "revision": "01dc0e2e057a05846bce",
+    "url": "/testProject/static/css/main.339304c1.chunk.css"
+  },
+  {
+    "revision": "51bdc9aec1cb993564b8",
+    "url": "/testProject/static/js/2.225a2dd8.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/testProject/static/js/2.225a2dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01dc0e2e057a05846bce",
+    "url": "/testProject/static/js/main.37e61ec8.chunk.js"
+  },
+  {
+    "revision": "bf66221f8297c41e11a0",
+    "url": "/testProject/static/js/runtime-main.d30e2b1c.js"
+  }
+]);
