d74b7708584920150ea9e901e7381602313884f1
mregydev <mregydev@gmail.com>
2020-10-28 07:27:27 +0200

    updated website

---
+++ task/precache-manifest.474aa057b9a23a3a6e4899d1a64ef13b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34eef78bd33fc0d8af7666276d31a04",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "342e40610b5e1a0d8713",
+    "url": "/task/static/css/main.0b74458e.chunk.css"
+  },
+  {
+    "revision": "4bc410528ff3f0e4b787",
+    "url": "/task/static/js/2.7729db7b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/task/static/js/2.7729db7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342e40610b5e1a0d8713",
+    "url": "/task/static/js/main.4e9f4dff.chunk.js"
+  },
+  {
+    "revision": "a79457ef7c1bcac53ca1",
+    "url": "/task/static/js/runtime-main.3d527374.js"
+  }
+]);
