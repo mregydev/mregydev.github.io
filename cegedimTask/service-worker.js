68a416c40b16f5eba5568baa4bf08ed303e871d8
alaa.hamed <alaa.hamed@cegedim.com>
2020-11-24 12:54:59 +0200

    code updates
    code updates

--- cegedimTask/service-worker.js
+++ cegedimTask/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.089f0da5b8c7308ddc3a98cb5b2d9601.js"
+  "/cegedimTask/precache-manifest.11ebe2fb82f459e7e088404185b06b30.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cegedimTask/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
